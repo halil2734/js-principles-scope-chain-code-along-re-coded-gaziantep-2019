438284a435849694595f872a66e0cc3e02074318
Halil güler <h.guler3717@gmail.com>
2019-08-30 12:12:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ function upperCaseCustomerName(){
   customerName =customerName.toUpperCase();
 }
 function setBestCustomer(){
-  
+  bestCustomer = 'not bob';
+}
+function overWriteBestCustomer(){
+  bestCustomer = 
 }
 
 
